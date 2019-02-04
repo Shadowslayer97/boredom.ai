e42b6da86321019280b374da7929abc480bd876f
Srirang97 <srirangsathyan17@gmail.com>
2019-02-04 16:25:07 +0530

    :

--- client/main.js
+++ client/main.js
@@ -1,27 +1,4 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-// import './main.html';
 import '../imports/ui/body.js';
-//
-// Template.hello.onCreated(function helloOnCreated() {
-//   // counter starts at 0
-//   this.counter = new ReactiveVar(0);
-// });
-//
-// Template.hello.helpers({
-//   list(){
-//     return [
-//       { text:"Hello there bot", orientation:"right" },
-//       { text:"Hello User", orientation:"left" },
-//       { text:"Hmm", orientation:"right" }
-//     ]
-//   }
-// });
-//
-// Template.hello.events({
-//   'click button'(event, instance) {
-//     // increment the counter when button is clicked
-//     instance.counter.set(instance.counter.get() + 1);
-//   },
-// });
--- imports/ui/body.js
+++ imports/ui/body.js
@@ -2,12 +2,37 @@ import { Template } from 'meteor/templating';
 
 import './body.html';
 
-Template.chatItem.helpers({
-  chats(){
-      return [
-      { text:"Hello there bot", orientation:"right" },
-      { text:"Hello User", orientation:"left" },
-      { text:"Hmm", orientation:"right" }
-    ]
+var chatArr = new ReactiveArray([
+    { text:"Hello there bot", orientation:"right",img:"right.png" },
+    { text:"Hello User", orientation:"left",img:"right.png" },
+    { text:"Hmm", orientation:"right",img:"right.png" }
+  ]);
+
+Template.chatList.onCreated(function chatListOnCreated() {
+})
+
+Template.chatList.helpers({
+  chats: function() {
+    return chatArr.list();
   }
+});
+
+Template.chatForm.helpers({
+  message() {
+    return Template.instance().message.get();
+  }
+})
+
+Template.chatForm.events({
+  'click button': function() {
+    // Message
+    var messageDict = {
+      text: document.getElementById('message').value,
+      orientation: "right",
+      img: "right.png"
+    }
+
+    chatArr.push(messageDict);
+    console.log(messageDict);
+  },
 })
