397b2ef65921c6f8ff2eda964185ee5645efc65c
Srirang97 <srirangsathyan17@gmail.com>
2019-02-04 16:48:22 +0530

    Feat:dialogflow intent called

--- server/main.js
+++ server/main.js
@@ -1,5 +1,22 @@
 import { Meteor } from 'meteor/meteor';
+var apiai = require('apiai');
 
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+var app = apiai("56ad813f1beb4cdd96b9c64886434add");
+
+var request = app.textRequest('what is your name?', {
+    sessionId: 'aidhuojas2kwe2sda'
+});
+
+request.on('response', function(response) {
+    console.log(response.result.fulfillment.speech);
+});
+
+request.on('error', function(error) {
+    console.log(error);
+});
+
+request.end();
